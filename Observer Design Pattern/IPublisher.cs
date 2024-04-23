bb489f07146f52721f13dd83a32cc2801089f507
NirmitBhatt <nirmitb97@gmail.com>
2024-04-23 20:27:48 +0200

    Added helpful comments

--- Observer Design Pattern/IPublisher.cs
+++ Observer Design Pattern/IPublisher.cs
@@ -1,3 +1,4 @@
+//Creating an IObservable interface that acts as a Subject
 public interface IPublisher{
     public void AddSubscriber(ISubscriber subscriber);
     public void RemoveSubscriber(ISubscriber subscriber);
--- Observer Design Pattern/ISubscriber.cs
+++ Observer Design Pattern/ISubscriber.cs
@@ -1,3 +1,4 @@
+//Creating an IObserver interface that acts as an Observer
 public interface ISubscriber{
     public void UpdateSubscriber();
 }
